4f7c1491fee26f9f4a647e880fa6e99cad5aab67
s-chang <smythchang@gmail.com>
2018-07-09 14:23:49 -0700

    a few additions

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 import bank.Bank;
+import java.util.*;
+
 
 public class Main{
     
 
     public static void main(String[] args){
-        Bank theBank;
+        Bank theBank = new Bank();
+        
+        
 
         
+    }
+
+    public int getRandomID(){
+        //0-900001
+        Random rand = new Random();
+        int id = rand(900000) + 100000;
+        return id;
     }
 }
